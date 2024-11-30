cadaba96f91d53717abdc77b12a0cc2c3bec5a18
limboden <imbodenlu@gmail.com>
2024-11-30 18:24:08 -0500

    oops, i wrote it in the wrong spot

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,12 +3,13 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
+
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
 
   isTokenExpired(token: string) {
