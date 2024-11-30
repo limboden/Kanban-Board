7ba78675984413a3932ffdbbc9a3f29ae55b0410
limboden <imbodenlu@gmail.com>
2024-11-30 18:23:16 -0500

    added getProfile, istokenexpired, and gettoken

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,7 +1,6 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
   try {
     const response = await fetch('auth/login', {
       method: 'POST',
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,18 +3,30 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
   }
-  
+
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token);
+      if (!exp) {
+        return true;
+      }
+      return Date.now() >= exp * 5000;
+    } catch (err) {
+      return true;
+    }
   }
 
-  getToken(): string {
+  getToken(): string | null {
     // TODO: return the token
+    return localStorage.getItem('id_token');
   }
 
   login(idToken: string) {
