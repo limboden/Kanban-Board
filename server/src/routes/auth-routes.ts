23afceed6cdb5f31a127b73c47b16596443adba2
limboden <imbodenlu@gmail.com>
2024-12-05 15:04:46 -0500

    checks if the bcrypt password matches the stores bcrypt password

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -10,10 +10,25 @@ export const login = async (req: Request, res: Response) => {
   try {
     const user: User | null = await User.findOne({ where: { username } });
     console.log("User found:", user);
-
+    // checks if user exists, if it doesn't, invalid username or password
     if (!user) {
       res.status(401).json({ error: 'Invalid username of password' });
+      return; // get out
     }
+
+    //checking password
+    const validPassword = await bcrypt.compare(password, user.password);
+    console.log("Password valid:", validPassword);
+
+
+    //invalid password
+    if (!validPassword) {
+      res.status(401).json({ error: 'Invalid username of password' });
+      return; //get out
+    }
+
+
+
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).json({ error: 'An error occurred while logging in' });
